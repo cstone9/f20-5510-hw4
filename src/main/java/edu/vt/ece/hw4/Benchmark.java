315e9a4707ae8ecc10fa87ed83ad61caab3a0e3c
ibalajiarun <balajia@vt.edu>
2019-10-23 17:59:19 -0400

    Abstracting run method to clear lock for each run

--- src/main/java/edu/vt/ece/hw4/Benchmark.java
+++ src/main/java/edu/vt/ece/hw4/Benchmark.java
@@ -6,11 +6,13 @@ import edu.vt.ece.hw4.bench.*;
 import edu.vt.ece.hw4.locks.ALock;
 import edu.vt.ece.hw4.locks.BackoffLock;
 import edu.vt.ece.hw4.locks.Lock;
+import edu.vt.ece.hw4.locks.MCSLock;
 
 public class Benchmark {
 
     private static final String ALOCK = "ALock";
     private static final String BACKOFFLOCK = "BackoffLock";
+    private static final String MCSLOCK = "MCSLock";
 
     public static void main(String[] args) throws Exception {
         String mode = args.length <= 0 ? "normal" : args[0];
@@ -19,62 +21,65 @@ public class Benchmark {
         int totalIters = (args.length <= 3 ? 64000 : Integer.parseInt(args[3]));
         int iters = totalIters / threadCount;
 
-        Lock lock = null;
-        switch (lockClass.trim()) {
-            case ALOCK:
-                lock = new ALock(threadCount);
-                break;
-            case BACKOFFLOCK:
-                lock = new BackoffLock(args[4]);
-                break;
-        }
-
-        switch (mode.trim().toLowerCase()) {
-            case "normal":
-                final Counter counter = new SharedCounter(0, lock);
-                for (int i = 0; i < 2; i++) {
-                    runNormal(counter, threadCount, iters);
-                }
-                break;
-            case "empty":
-                for (int i = 0; i < 2; i++) {
-                    runEmptyCS(lock, threadCount, iters);
-                }
-                break;
-            case "long":
-                for (int i = 0; i < 2; i++) {
-                    runLongCS(lock, threadCount, iters);
-                }
-                break;
-            case "barrier":
-                Barrier b = new TTASBarrier();
-                throw new UnsupportedOperationException("Complete this.");
-            default:
-                throw new UnsupportedOperationException("Implement this");
-        }
+        run(args, mode, lockClass, threadCount, iters);
 
     }
 
-    private static void runNormal(Counter counter, int threadCount, int iters) throws Exception {
+    private static void run(String[] args, String mode, String lockClass, int threadCount, int iters) throws Exception {
+        for (int i = 0; i < 5; i++) {
+            Lock lock = null;
+            switch (lockClass.trim()) {
+                case ALOCK:
+                    lock = new ALock(threadCount);
+                    break;
+                case BACKOFFLOCK:
+                    lock = new BackoffLock(args[4]);
+                    break;
+                case MCSLOCK:
+                    lock = new MCSLock();
+                    break;
+            }
 
+            switch (mode.trim().toLowerCase()) {
+                case "normal":
+                    lock = new MCSLock();
+                    final Counter counter = new SharedCounter(0, lock);
+                    runNormal(counter, threadCount, iters);
+                    break;
+                case "empty":
+                    runEmptyCS(lock, threadCount, iters);
+                    break;
+                case "long":
+                    runLongCS(lock, threadCount, iters);
+                    break;
+                case "barrier":
+                    Barrier b = new TTASBarrier();
+                    throw new UnsupportedOperationException("Complete this.");
+                default:
+                    throw new UnsupportedOperationException("Implement this");
+            }
+        }
+    }
+
+    private static void runNormal(Counter counter, int threadCount, int iters) throws Exception {
         final TestThread[] threads = new TestThread[threadCount];
         TestThread.reset();
 
-        for(int t=0; t<threadCount; t++) {
+        for (int t = 0; t < threadCount; t++) {
             threads[t] = new TestThread(counter, iters);
         }
 
-        for(int t=0; t<threadCount; t++) {
+        for (int t = 0; t < threadCount; t++) {
             threads[t].start();
         }
 
         long totalTime = 0;
-        for(int t=0; t<threadCount; t++) {
+        for (int t = 0; t < threadCount; t++) {
             threads[t].join();
             totalTime += threads[t].getElapsedTime();
         }
 
-        System.out.println("Average time per thread is " + totalTime/threadCount + "ms");
+        System.out.println("Average time per thread is " + totalTime / threadCount + "ms");
     }
 
     private static void runEmptyCS(Lock lock, int threadCount, int iters) throws Exception {
